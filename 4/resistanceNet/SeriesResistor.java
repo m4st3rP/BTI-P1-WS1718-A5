5cae2989f8e6f4a6e2a778d4f30b2c0b6995dd36
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-27 00:51:33 +0100

    Update SeriesResistor.java

--- 4/resistanceNet/SeriesResistor.java
+++ 4/resistanceNet/SeriesResistor.java
@@ -1,15 +1,35 @@
 package resistanceNet;
 
 public class SeriesResistor extends ComposedResistor {
-    
+
     SeriesResistor(ResistanceNet... resistanceNet) {
         super(resistanceNet);
+        this.resistance = calculateResistance(resistanceNet);
     }
-    
-    
-    
+
+
+
     @Override
     String getCircuit() {
-        return "";
+        String returnString = "(";
+
+        for (int i = 0; i < getSubNets().length - 1; i++) {
+            returnString += getSubNets()[i].getCircuit() + "+";
+        }
+
+        return returnString + getSubNets()[getSubNets().length - 1].getCircuit() + ")";
+    }
+
+    @Override
+    int getNumberOfResistors() {
+        return getSubNets().length;
+    }
+
+    double calculateResistance(ResistanceNet... resistanceNet) {
+        double newResistance = 0;
+        for (ResistanceNet r : resistanceNet) {
+            newResistance += r.getResistance();
+        }
+        return newResistance;
     }
 }
