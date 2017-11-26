b29e4b1908516725543ce000383092e25f8b4ff0
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-26 15:22:19 +0100

    Update CardProcessor.java

--- 1/cardProcessor/CardProcessor.java
+++ 1/cardProcessor/CardProcessor.java
@@ -1,38 +1,37 @@
 package cardProcessor;
 
-import java.util.*;
-
-import cards.*;
-import cards.Card.*;
-import static cards.Card.*;
-import static cards.Card.Constant.*;
-
+import java.util.Stack;
+import cards.Card;
+import cards.Deck;
 
 /**
  * CardProcessor - see task
  */
 public class CardProcessor {
     /**
-     * Die Methode reverseOrder() zieht solange Karten von einem Kartenstapel  bis eine gewuenschte Karte gezogen wird
-     * und gibt danach alle gezogenen Karten in umgekehrter Reihenfolge wieder aus.
+     * Die Methode reverseOrder() zieht solange Karten von einem Kartenstapel bis eine gewuenschte Karte
+     * gezogen wird und gibt danach alle gezogenen Karten in umgekehrter Reihenfolge wieder aus.
      *
-     * @param deck              der Kartenstapel von dem die Karten gezogen werden.
-     * @param lastCard          die gewuenschte Karte, die die Ziehung beendet.
-     * @param dbgOutputEnable   true schaltet Kontrollausgabe an und false aus.
+     * @param deck der Kartenstapel von dem die Karten gezogen werden.
+     * @param lastCard die gewuenschte Karte, die die Ziehung beendet.
+     * @param dbgOutputEnable true schaltet Kontrollausgabe an und false aus.
      */
-    // HHH      HHH   III   EEEEEEEEEEEE   RRRRRRRRRRR          <<<                                  !!!
-    // HHH      HHH   III   EEEEEEEEEEEE   RRRRRRRRRRRR        <<<                                   !!!
-    // HHH      HHH   III   EEE            RRR      RRR       <<<                                    !!!
-    // HHH      HHH   III   EEE            RRR      RRR      <<<                                     !!!
-    // HHHHHHHHHHHH   III   EEEEEEEEEEEE   RRRRRRRRRRR      <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<     !!!
-    // HHHHHHHHHHHH   III   EEEEEEEEEEEE   RRRRRRRRRR       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<     !!!
-    // HHH      HHH   III   EEE            RRR   RRR         <<<                                     !!!
-    // HHH      HHH   III   EEE            RRR    RRR         <<<
-    // HHH      HHH   III   EEEEEEEEEEEE   RRR     RRR         <<<                                   !!!
-    // HHH      HHH   III   EEEEEEEEEEEE   RRR      RRR         <<<                                  !!!
-    //
-    // Fuegen Sie hier Ihren Code ein
-    // bzw. ersetzen Sie diesen Kommentar durch Ihren Code.
-    // ...
-    
-}//class
+    public void reverseOrder(Deck deck, Card lastCard, boolean dbgOutputEnable) {
+        Card currentCard;
+        Stack<Card> cardStack = new Stack<Card>();
+
+        // draw a card and put it on the stack until we get the card we want
+        do {
+            currentCard = deck.deal();
+            if (dbgOutputEnable)
+                System.out.print(currentCard);
+            cardStack.push(currentCard);
+        } while (!currentCard.equals(lastCard));
+        System.out.println();
+        // pop and print last element of stack until it's empty
+        while (!cardStack.empty()) {
+            System.out.print(cardStack.pop());
+        }
+    }
+
+}// class
