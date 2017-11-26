98c8ea3093c2e2a33f955ee8c9a153f79e94b02c
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-26 15:34:37 +0100

    Update CardProcessor.java

--- 2/cardProcessor/CardProcessor.java
+++ 2/cardProcessor/CardProcessor.java
@@ -1,38 +1,26 @@
 package cardProcessor;
 
-
-import java.util.*;
-
-import cards.*;
-import cards.Card.*;
-import static cards.Card.*;
-import static cards.Card.Constant.*;
-
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import cards.Card;
 
 public class CardProcessor {
-    
+
     /**
-     * Die Methode removeDuplicates() entfernt Doppelte aus den uebergebenen Karten.
-     * Welche Karte konkret entfernt wird ist egal.
-     * Wichtig ist, dass es bei den als Ergebnis abgelieferten Katen keine Doppelten gibt, aber sonst jede Karten erhalten bleibt. 
+     * Die Methode removeDuplicates() entfernt Doppelte aus den uebergebenen Karten. Welche Karte
+     * konkret entfernt wird ist egal. Wichtig ist, dass es bei den als Ergebnis abgelieferten Katen
+     * keine Doppelten gibt, aber sonst jede Karten erhalten bleibt.
      *
-     * @param cards     die uebergebenen Karten aus denen die Doppelten entfernt werden.
+     * @param cards die uebergebenen Karten aus denen die Doppelten entfernt werden.
      * 
-     * @return          die Karten befreit von Doppelten.
+     * @return die Karten befreit von Doppelten.
      */
-    // HHH      HHH   III   EEEEEEEEEEEE   RRRRRRRRRRR          <<<                                  !!!
-    // HHH      HHH   III   EEEEEEEEEEEE   RRRRRRRRRRRR        <<<                                   !!!
-    // HHH      HHH   III   EEE            RRR      RRR       <<<                                    !!!
-    // HHH      HHH   III   EEE            RRR      RRR      <<<                                     !!!
-    // HHHHHHHHHHHH   III   EEEEEEEEEEEE   RRRRRRRRRRR      <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<     !!!
-    // HHHHHHHHHHHH   III   EEEEEEEEEEEE   RRRRRRRRRR       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<     !!!
-    // HHH      HHH   III   EEE            RRR   RRR         <<<                                     !!!
-    // HHH      HHH   III   EEE            RRR    RRR         <<<
-    // HHH      HHH   III   EEEEEEEEEEEE   RRR     RRR         <<<                                   !!!
-    // HHH      HHH   III   EEEEEEEEEEEE   RRR      RRR         <<<                                  !!!
-    //
-    // Fuegen Sie hier Ihren Code ein
-    // bzw. ersetzen Sie diesen Kommentar durch Ihren Code.
-    // ...
-    
-}//class
+    public Card[] removeDuplicates(Card... cardsArray) {
+        // Transform Array to Set
+        Set<Card> cardsSet = new HashSet<Card>(Arrays.asList(cardsArray));
+        // Transform back to Array
+        cardsArray = cardsSet.toArray(new Card[cardsSet.size()]);
+        return cardsArray;
+    }
+}// class
