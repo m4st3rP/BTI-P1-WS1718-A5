5a155a95d44c2ccf5a3dbeb6137e78756df0640a
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-27 00:51:56 +0100

    Update ResistanceNet.java

--- 4/resistanceNet/ResistanceNet.java
+++ 4/resistanceNet/ResistanceNet.java
@@ -1,19 +1,26 @@
 package resistanceNet;
 
 public class ResistanceNet {
-    
-    //Returns total resistance of the network
+    protected double resistance;
+
+    ResistanceNet() {}
+
+    ResistanceNet(double resistance) {
+        this.resistance = resistance;
+    }
+
+    // Returns total resistance of the network
     double getResistance() {
-        return 0.0;
+        return this.resistance;
     }
-    
-    //Returns amount of simple Resistors in the Network
+
+    // Returns amount of simple Resistors in the Network
     int getNumberOfResistors() {
-        return 0;
+        return 1;
     }
-    
-    //Returns a Description of the Network as String
+
+    // Returns a Description of the Network as String
     String getCircuit() {
-        return "";
+        return "The resistance of the network is: " + getResistance() + " Ohm\nThe amount of resistors are: " + getNumberOfResistors();
     }
 }
