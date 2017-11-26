36ce90c82b67e7df6c53e57b36fced118b67c5dc
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-27 00:52:58 +0100

    Create MyTestFrame.java

---
+++ 4/resistanceNet/MyTestFrame.java
@@ -0,0 +1,30 @@
+package resistanceNet;
+
+public class MyTestFrame {
+
+    public static void main(String[] args) {
+        Resistor r1 = new Resistor("r1", 100);
+        Resistor r2 = new Resistor("r2", 200);
+        Resistor r3 = new Resistor("r3", 300);
+        Resistor r4 = new Resistor("r4", 400);
+        Resistor r5 = new Resistor("r5", 500);
+        Resistor r6 = new Resistor("r6", 600);
+
+        ParallelResistor r13 = new ParallelResistor(r1, r3);
+        SeriesResistor r132 = new SeriesResistor(r13, r2);
+
+        SeriesResistor r45 = new SeriesResistor(r4, r5);
+
+        ParallelResistor r132456 = new ParallelResistor(r132, r45, r6);
+
+        System.out.println("Gesamtwiderstand: " + r132456.getResistance());
+        System.out.println("Sollte: 155.91");
+        System.out.println();
+        System.out.println("Anzahl Widerstände: " + r132456.getNumberOfResistors());
+        System.out.println("Sollte: 6");
+        System.out.println();
+        System.out.println(r132456.getCircuit());
+        System.out.println("Sollte: (((R1|R3)+R2)|(R4+R5)|R6)");
+    }
+
+}
