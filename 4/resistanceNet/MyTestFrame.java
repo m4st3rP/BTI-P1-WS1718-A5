f92de72ffab56083aed1021cb4913d38f2cd80ec
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-27 17:27:37 +0100

    Update MyTestFrame.java

--- 4/resistanceNet/MyTestFrame.java
+++ 4/resistanceNet/MyTestFrame.java
@@ -3,6 +3,12 @@ package resistanceNet;
 public class MyTestFrame {
 
     public static void main(String[] args) {
+        test1();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        test2();
+    }
+
+    public static void test1() {
         Resistor r1 = new Resistor("r1", 100);
         Resistor r2 = new Resistor("r2", 200);
         Resistor r3 = new Resistor("r3", 300);
@@ -27,4 +33,24 @@ public class MyTestFrame {
         System.out.println("Sollte: (((R1|R3)+R2)|(R4+R5)|R6)");
     }
 
+    public static void test2() {
+        Resistor r1 = new Resistor("r1", 100);
+        Resistor r2 = new Resistor("r2", 200);
+        Resistor r3 = new Resistor("r3", 300);
+        Potentiometer r4 = new Potentiometer("r4");
+        Resistor r5 = new Resistor("r5", 500);
+        Resistor r6 = new Resistor("r6", 600);
+
+        ParallelResistor r13 = new ParallelResistor(r1, r3);
+        SeriesResistor r132 = new SeriesResistor(r13, r2);
+
+        SeriesResistor r45 = new SeriesResistor(r4, r5);
+
+        ParallelResistor r132456 = new ParallelResistor(r132, r45, r6);
+
+        for (int i = 0; i <= 4000; i += 400) {
+            r4.setResistance(i);
+            System.out.println("Gesamtwiderstand(" + i + "): " + r132456.getResistance());
+        }
+    }
 }
